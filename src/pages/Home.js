44a5c4ce6088e39b65263a516aaaf64fa0aaa158
Mirja Pirinen <mirja.pirinen@hotmail.com>
2024-03-29 16:43:51 +0200

    1st image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import '../App.css';
+
 export const Home = () => {
     return (
+        <>
         <div>Hello from home</div>
+        <head className="App-header main">
+        <div className="etusivu">
+          
+          
+            <h1 style={{fontSize: '100px'}}>Golfmatkat<br />
+            hyvässä seurassa</h1>
+            
+            <h4>Koe upeat golfkohteet ympäri maailman!</h4>
+            
+            </div>
+        </head>
+        </>
     )
 }
