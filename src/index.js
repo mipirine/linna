29e0102fdba9220fb6ef0cc32b2e898d620a02f6
Mirja Pirinen <mirja.pirinen@hotmail.com>
2024-03-30 09:09:39 +0200

    bootstrap add

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 ReactDOM.render(<App />, document.getElementById('root'));
